67910b97aab0cd44f0cf092775c882c02d3ae6bc
Meng Li <34143965+Meng6@users.noreply.github.com>
2023-11-20 22:49:31 +0000

    Update demos based on geist v0.0.2

--- demo/02-tro-examples/03-skope-lbda-processing/run.sh
+++ demo/02-tro-examples/03-skope-lbda-processing/run.sh
@@ -20,13 +20,13 @@ geist report --outputroot products << END_TEMPLATE
         {%- map isfilepath=False, mappings="mappings.json" as query_tro_shorten %} {% query_tro_str %} {% endmap %}
         {%- set query_tro = query_tro_shorten | json2df %}
         {% for _, row in query_tro.iterrows() %}
-        <u>{{ row["tro_name"][1:-1] }}</u>
+        <u>{{ row["tro_name"] }}</u>
         <ul>
-            <li>TRO ID: {{ row["tro_id"][1:-1] }}</li>
-            <li>TRO Description: {{ row["tro_descr"][1:-1] }}</li>
-            <li>Digital artifacts: {{ row["num_of_artifacts"][1:-1] }}</li>
-            <li>Artifact artifact arrangements: {{ row["num_of_arrs"][1:-1] }}</li>
-            <li>Trusted Research Performances (TRPs): {{ row["num_of_trps"][1:-1] }}</li>
+            <li>TRO ID: {{ row["tro_id"] }}</li>
+            <li>TRO Description: {{ row["tro_descr"] }}</li>
+            <li>Digital artifacts: {{ row["num_of_artifacts"] }}</li>
+            <li>Artifact artifact arrangements: {{ row["num_of_arrs"] }}</li>
+            <li>Trusted Research Performances (TRPs): {{ row["num_of_trps"] }}</li>
         </ul>
         {% endfor %}
 
@@ -34,11 +34,11 @@ geist report --outputroot products << END_TEMPLATE
         {%- map isfilepath=False, mappings="mappings.json" as query_trs_shorten %} {% query_trs_str %} {% endmap %}
         {%- set query_trs = query_trs_shorten | json2df %}
         {% for _, row in query_trs.iterrows() %}
-        <u>{{ row["trs_name"][1:-1] }}</u>
+        <u>{{ row["trs_name"] }}</u>
         <ul>
-            <li>TRS ID: {{ row["trs_id"][1:-1] }}</li>
-            <li>TRS Description: {{ row["trs_descr"][1:-1] }}</li>
-            <li>Capabilities: {{ row["num_of_capabilities"][1:-1] }} (see below)</li>
+            <li>TRS ID: {{ row["trs_id"] }}</li>
+            <li>TRS Description: {{ row["trs_descr"] }}</li>
+            <li>Capabilities: {{ row["num_of_capabilities"] }} (see below)</li>
         </ul>
         {% endfor %}
         {%- table mappings="mappings.json" %}{% query_trs_capability_str %}{% endtable %}
@@ -67,15 +67,15 @@ geist report --outputroot products << END_TEMPLATE
             }
             ORDER BY ?arrangement
         {% endquery %}
-        {%- set query_arrangement = query_arrangement_str | json2df %}
-        {%- map isfilepath=False, mappings="mappings.json" as query_arrangement_shorten_str %} {{ query_arrangement_str }} {% endmap %}
-        {%- set query_arrangement_shorten = query_arrangement_shorten_str | json2df %}
-        {% for idx, row in query_arrangement_shorten.iterrows() %}
-        <u>{{ row["name"][1:-1] }}</u>
+
+        {%- map isfilepath=False, mappings="mappings.json", on="id" as query_arrangement_shorten %} {{ query_arrangement_str }} {% endmap %}
+        {%- set query_arrangement = query_arrangement_shorten | json2df %}
+        {% for idx, row in query_arrangement.iterrows() %}
+        <u>{{ row["name"] }}</u>
         <ul>
-            <li>Arrangement ID: {{ row["id"][1:-1] }}</li>
-            <li>Arrangement Description: {{ row["descr"][1:-1] }}</li>
-            <li>Digital artifacts: {%- table mappings="mappings.json" %}{% query_artifacts_by_arrangement_str query_arrangement.loc[idx, "arrangement"] %}{% endtable %}</li>
+            <li>Arrangement ID: {{ row["id"] }}</li>
+            <li>Arrangement Description: {{ row["descr"] }}</li>
+            <li>Digital artifacts: {%- table mappings="mappings.json" %}{% query_artifacts_by_arrangement_str row["arrangement"] %}{% endtable %}</li>
         </ul>
         {% endfor %}
 
